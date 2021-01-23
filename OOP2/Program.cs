05b6c0317051c27e657f4f9946bbeb9ec997f6c7
Doğan Muş <doganmus87@gmail.com>
2021-01-24 00:16:51 +0300

    240122021 kamp

---
+++ OOP2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "123456";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678900";
+
+            //kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "12345678911";
+
+
+
+
+            //Gerçek Müşteri - Tüzel Müşteri birbiri yerine kullanılamazlar.
+            //SOLID yazılım geliştirme teknikleri
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+
+            //new varsa bellekteki referans nosu,
+            //Musteri iki tip müşterinin referansını tutabiliyor.
+        }
+    }
+}
---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //interfaceleri nerelerde kullanırız?
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BiseyYap();
+       
+    }
+}
